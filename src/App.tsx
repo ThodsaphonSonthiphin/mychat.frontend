3287c79cb884bd329744e1944fe2bf9ea10642f9
Liusp <thodsaphonsp@gmail.com>
2022-09-07 07:53:52 +0700

    add chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, {useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Lobby} from "./components/Loby";
+import {HubConnection, HubConnectionBuilder, LogLevel} from "@microsoft/signalr";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    const [connection, setConnection] = useState<HubConnection>();
+
+    async function JoinRoom(user: string, room: string) {
+
+
+        try {
+            const roomConnection = new HubConnectionBuilder().withUrl("https://localhost:7050/chat")
+                .configureLogging(LogLevel.Debug)
+                .build();
+
+
+            roomConnection.on("ReceiveMessage", (user, message) => {
+                console.log(`message receive: ${message}`)
+            });
+            
+           
+
+            await roomConnection.start();
+
+            await roomConnection.invoke("JoinRoom", {user, room});
+            setConnection(roomConnection)
+
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    return (<div className={'app'}>
+        <h2>MyChat</h2>
+        {/*<hr className={"line"}/>*/}
+        <Lobby joinRoom={JoinRoom}></Lobby>
+    </div>);
+    
 }
 
 export default App;
---
+++ src/components/Loby.tsx
@@ -0,0 +1,20 @@
+﻿import {Button, Form} from "react-bootstrap";
+import {useState} from "react";
+export interface ILobby{
+    joinRoom:(user:string,room:string)=>void
+}
+
+export function Lobby (props:ILobby){
+    const  [user,setUser] = useState<string>("");
+    const  [room,setRoom] = useState<string>("");
+    return (<Form className={"lobby"} onSubmit={e=>{
+        e.preventDefault();
+        props.joinRoom(user,room)
+    }}>
+        <Form.Group>
+            <Form.Control placeholder={"name"} onChange={e=>setUser(e.target.value)}></Form.Control>
+            <Form.Control placeholder={"room"} onChange={e=>setRoom(e.target.value)}></Form.Control>
+            <Button variant={"success"} type={"submit"}>Join</Button>
+        </Form.Group>
+    </Form>);
+}
